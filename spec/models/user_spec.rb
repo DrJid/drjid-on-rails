2da432a34b5f5316e62cb2d72dd807bd58cd2cbd
Moujaled Maijid <maijid@gmail.com>
2012-06-22 14:19:19 -0500

    Modeling user part uno

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,84 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+require 'spec_helper'
+
+describe User do
+
+	before do
+	 	 @user = User.new( name: "Example User", email: "user@example.com", password: "foobar", password_confirmation:"foobar") 
+	end
+
+	subject { @user }
+
+	it {should respond_to(:name)}
+	it {should respond_to(:email)}
+	it {should respond_to(:password_digest)}
+	it {should respond_to(:password)}
+	it {should respond_to(:password_confirmation)}
+
+	describe "when name is not present" do
+		before { @user.name = ' '}
+		it { should_not be_valid }
+	end
+
+	describe "when email is not present" do 
+		before { @user.email = ' ' }
+		it { should_not be_valid}
+	end
+
+	describe "when name is too long" do
+		before { @user.name = 'a' * 51 }
+		it {should_not be_valid}
+	end
+
+	describe "when email format is invalid" do
+		it "should be invalid" do
+			addresses = %w[user@foo,com user_atfoo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+			addresses.each do |invalidaddress|
+				@user.email = invalidaddress
+				@user.should_not be_valid
+			end
+		end
+	end
+
+	describe "when email format is valid"  do
+		it "should be valid" do
+			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.last@foo.jp a+b@baz.cn]
+			addresses.each do |valid_address|
+				@user.email = valid_address
+				@user.should be_valid
+			end
+		end
+	end
+		
+	describe "when email is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
+		it {should_not be_valid}
+	end
+
+
+	describe "when password is not present" do
+		before { @user.password = @user.password_confirmation = " " }
+		it {should_not be_valid}
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
+		it {should_not be_valid}
+	end
+
+end
