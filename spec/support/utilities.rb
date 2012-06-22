eb6cb1b9597e94416cb8f364e07c56e32f0b48cf
Moujaled Maijid <maijid@gmail.com>
2012-06-22 01:17:27 -0500

    Onto 6

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,10 @@
-def full_title(page_title)
-	base_title = "DrJid On Rails"
-	if page_title.empty? 
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+# def full_title(page_title)
+# 	base_title = "DrJid On Rails"
+# 	if page_title.empty? 
+# 		base_title
+# 	else
+# 		"#{base_title} | #{page_title}"
+# 	end
+# end
+
+include ApplicationHelper
