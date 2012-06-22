e534e6e93d1340744ec8159e52088c2b064e53eb
Moujaled Maijid <maijid@gmail.com>
2012-06-22 16:11:00 -0500

    Basic User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@
 class User < ActiveRecord::Base
 
 	#This tells us which are accessible to users.
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
@@ -20,6 +21,10 @@ class User < ActiveRecord::Base
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates( :email, presence: true, format: {with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false} )
+
+  validates( :password, presence: true, length: { minimum: 6 } )
+
+  validates :password_confirmation, presence: true
 
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
 	it {should respond_to(:password_digest)}
 	it {should respond_to(:password)}
 	it {should respond_to(:password_confirmation)}
+	it {should respond_to(:authenticate)}
 
 	describe "when name is not present" do
 		before { @user.name = ' '}
@@ -80,5 +81,49 @@ describe User do
 		before { @user.password_confirmation = "mismatch" }
 		it {should_not be_valid}
 	end
+
+	describe "when password confirmation is nil" do
+		before { @user.password_confirmation = nil }
+		it {should_not be_valid}
+	end
+
+	describe "with a password that is too short" do
+		before { @user.password = @user.password_confirmation = "a" * 5 }
+		it {should be_invalid}
+	end
+
+	describe "return value of authenticate method" do
+		before { @user.save }
+		let(:found_user) { User.find_by_email(@user.email) }
+
+		describe "with valid password" do
+			it {should == found_user.authenticate(@user.password) }
+		end
+
+		describe "with invalid password" do
+			let (:user_for_invalid_password) {found_user.authenticate("invalid") }
+
+			it {should_not == user_for_invalid_password }
+			specify { user_for_invalid_password.should be_false }
+		end
+	end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
