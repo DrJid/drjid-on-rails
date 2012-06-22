5a3054806833c33ea0ef4749b01c9e2640018594
Moujaled Maijid <maijid@gmail.com>
2012-06-21 23:18:27 -0500

    auto paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,18 +8,18 @@ describe "StaticPages" do
 
 	describe "Home page" do
 
-		it "should have the h1 'DrJid Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => "DrJid Home")
+		it "should have the h1 'DrJid'" do
+			visit root_path
+			page.should have_selector('h1', :text => "DrJid")
 		end
 
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', :text => "#{base_title}")
 		end
 
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
@@ -28,12 +28,12 @@ describe "StaticPages" do
 	describe "Help Page" do 
 
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
@@ -42,24 +42,24 @@ describe "StaticPages" do
 	describe "About Page" do
 
 		it "should have the h1 'About'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'About')
 		end
 
 		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title', :text => "#{base_title} | About")
 		end
 	end
 
 	describe "Contact Page" do
 		it "should have the h1 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('h1', :text => 'Contact')
 		end
 
 		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			page.should have_selector('title', :text => "#{base_title} | Contact")
 		end
 	end
