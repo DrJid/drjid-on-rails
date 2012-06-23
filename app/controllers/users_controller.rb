8c03e1427585c3804b3fe37a2f0404503b2dfc74
Moujaled Maijid <maijid@gmail.com>
2012-06-22 20:52:45 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
 
+
+
+  def show
+  	#use Params to retrieve the user id. 
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	#Will have to put @user in here at some point. 
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		#Handle successful save
+  		flash[:success] = "Welcome to DrJid's first Rails App!"
+  		redirect_to @user #We can omit user_path
+  	else
+  		render 'new'
+  	end
   end
   
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+
+FactoryGirl.define do 
+	factory :user do
+		name		"Maijid"
+		email		"maijid@gmail.com"
+		password	"secret"
+		password_confirmation	"secret"
+	end
+end
